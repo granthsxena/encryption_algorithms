a77132f7dcd57690741d04197d4f82142762078a
Granth Saxena <137663384+granthsxena@users.noreply.github.com>
2023-07-09 09:45:01 +1000

    a simple implementation of the CBC using python libraries and an initialising vector

---
+++ encryption5.py
@@ -0,0 +1,16 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
+def encrypt_aes_cbc(plaintext, key):
+    iv = get_random_bytes(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
+    return iv + ciphertext
+
+def decrypt_aes_cbc(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    ciphertext = ciphertext[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return plaintext
