0f07f189a1fbf18eb02da9fc479dcd3813be3e48
Granth Saxena <137663384+granthsxena@users.noreply.github.com>
2023-07-09 09:49:22 +1000

    adding a decryption algorithm and removing comments temporarily

--- encryption.py
+++ encryption.py
@@ -5,23 +5,31 @@ def convert_letter(letter):
         return chr(ord('Z') - (ord(letter) - ord('A')))
     else:
         return letter
-
-
+        
 def encrypt_1(text):
-    #basic idea for this encryption algo is to convert odd characters into integers
-    # and even characters in to mirror characters (for ex. a becomes z)
-    #flip the final string
     answer = []
     for i in range(len(text)):
-        if i % 2:  # odd
+        if i % 2:
             answer.append(str(ord(text[i]) - 96))
         else:
             answer.append(convert_letter(text[i]))
 
     final = "".join(answer)
-    final = final[::-1]  # Flip the final string
+    final = final[::-1]
 
     return final
 
+def decrypt_1(text):
+    text = text[::-1]
 
-print(encrypt_1("cant read this even if you tried"))
+    answer = []
+    i = 0
+    while i < len(text):
+        if text[i].isdigit():
+            answer.append(chr(int(text[i]) + 96))
+            i += 1
+        else:
+            answer.append(convert_letter(text[i]))
+        i += 1
+
+    return "".join(answer)
